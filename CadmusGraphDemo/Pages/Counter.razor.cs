b02deb5310cae0853b556485dd0965cb3c03cb3b
Myrmex <dfusi@hotmail.com>
2022-05-22 09:50:32 +0200

    Basic demo UI

--- CadmusGraphDemo/Pages/Counter.razor.cs
+++ CadmusGraphDemo/Pages/Counter.razor.cs
@@ -1,12 +1,92 @@
-﻿namespace CadmusGraphDemo.Pages
+﻿using Cadmus.Graph;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+
+namespace CadmusGraphDemo.Pages
 {
     public partial class Counter
     {
-        private int currentCount = 0;
+        private readonly JsonNodeMapper _mapper;
+        public MappingModel Mapping { get; set; }
 
-        private void IncrementCount()
+        public Counter()
         {
-            currentCount++;
+            _mapper = new();
+            Mapping = new()
+            {
+                Input = LoadResourceText("Events.json"),
+                Mappings = LoadResourceText("Mappings.json")
+            };
+        }
+
+        private static Stream GetResourceStream(string name)
+        {
+            return Assembly.GetExecutingAssembly()!
+                .GetManifestResourceStream($"CadmusGraphDemo.Assets.{name}")!;
+        }
+
+        private static string LoadResourceText(string name)
+        {
+            using StreamReader reader = new(GetResourceStream(name),
+                Encoding.UTF8);
+            return reader.ReadToEnd();
+        }
+
+        private static IList<NodeMapping> LoadMappings(string json)
+        {
+            JsonSerializerOptions options = new()
+            {
+                AllowTrailingCommas = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                PropertyNameCaseInsensitive = true
+            };
+            options.Converters.Add(new NodeMappingOutputJsonConverter());
+
+            return JsonSerializer.Deserialize<IList<NodeMapping>>(json,
+                options) ?? Array.Empty<NodeMapping>();
+        }
+
+        private void Map()
+        {
+            Mapping.Error = null;
+            if (string.IsNullOrEmpty(Mapping.Input) ||
+                string.IsNullOrEmpty(Mapping.Mappings))
+            {
+                return;
+            }
+
+            try
+            {
+                GraphSet set = new();
+                IList<NodeMapping> mappings = LoadMappings(Mapping.Mappings);
+                int i = 0;
+                foreach (NodeMapping mapping in mappings)
+                {
+                    _mapper.Map($"m_{i++}", Mapping.Input, mapping, set);
+                }
+                StringBuilder sb = new();
+
+                sb.AppendFormat("NODES={0}", set.Nodes.Count);
+                foreach (UriNode node in set.Nodes)
+                {
+                    sb.AppendLine(node.ToString());
+                }
+
+                sb.AppendFormat("TRIPLES={0}", set.Triples.Count);
+                foreach (UriTriple triple in set.Triples)
+                {
+                    sb.AppendLine(triple.ToString());
+                }
+
+                Mapping.Output = sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+                Mapping.Error = ex.Message;
+            }
         }
     }
 }
---
+++ CadmusGraphDemo/Pages/MappingModel.cs
@@ -0,0 +1,10 @@
+﻿namespace CadmusGraphDemo.Pages
+{
+    public class MappingModel
+    {
+        public string? Input { get; set; }
+        public string? Mappings { get; set; }
+        public string? Output { get; set; }
+        public string? Error { get; set; }
+    }
+}
