e41a7ddcbe90f9bf94612070da30719b882224ee
Myrmex <dfusi@hotmail.com>
2022-06-12 13:45:33 +0200

    Demo API infrastructure

--- Cadmus.Graph.Api/Program.cs
+++ Cadmus.Graph.Api/Program.cs
@@ -1,46 +1,118 @@
 using Cadmus.Graph.MySql;
 using Cadmus.Index.Sql;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using System.Diagnostics;
+using Microsoft.Extensions.Hosting;
+using Serilog.Events;
+using Cadmus.Graph.Api.Services;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace Cadmus.Graph.Api
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        private static void DumpEnvironmentVars()
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddScoped<IGraphRepository>(provider =>
+            Console.WriteLine("ENVIRONMENT VARIABLES:");
+            IDictionary dct = Environment.GetEnvironmentVariables();
+            List<string> keys = new();
+            var enumerator = dct.GetEnumerator();
+            while (enumerator.MoveNext())
             {
-                string cs = provider.GetService<IConfiguration>()
-                    .GetConnectionString("Default");
-                var repository = new MySqlGraphRepository();
-                repository.Configure(new SqlOptions
-                {
-                    ConnectionString = cs
-                });
-                return repository;
-            });
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                keys.Add(((DictionaryEntry)enumerator.Current).Key.ToString()!);
             }
 
-            app.UseAuthorization();
+            foreach (string key in keys.OrderBy(s => s))
+                Console.WriteLine($"{key} = {dct[key]}");
+        }
 
-            app.MapControllers();
+        public static void Main(string[] args)
+        {
+            // setup logger: see instructions at:
+            // https://github.com/datalust/dotnet6-serilog-example
 
-            app.Run();
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
+            Log.Information("Starting up");
+
+            try
+            {
+                Log.Information("Starting Cadmus Graph API host");
+                DumpEnvironmentVars();
+
+                WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+                // more config sources
+                builder.Configuration.AddEnvironmentVariables();
+                builder.Configuration.AddCommandLine(args);
+
+                // add services to the container
+                builder.Services.AddScoped<IGraphRepository>(provider =>
+                {
+                    var config = provider.GetService<IConfiguration>();
+                    string cst = config.GetConnectionString("Template");
+                    var repository = new MySqlGraphRepository();
+                    repository.Configure(new SqlOptions
+                    {
+                        ConnectionString = string.Format(cst,
+                            config.GetValue<string>("DatabaseName") ?? "cadmus-graph")
+                    });
+                    return repository;
+                });
+
+                builder.Services.AddControllers();
+
+                // more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                // setup log (config is read from appsettings.json under Serilog)
+                builder.Host.UseSerilog((ctx, lc) => lc
+                    .WriteTo.Console()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+//#if DEBUG
+//                    .WriteTo.File("cadmus-graph-log.txt", rollingInterval: RollingInterval.Day)
+//#endif
+                    .ReadFrom.Configuration(ctx.Configuration));
+
+                // seed service
+                // https://andrewlock.net/running-async-tasks-on-app-startup-in-asp-net-core-3/
+                builder.Services.AddHostedService<DatabaseSeedService>();
+
+                WebApplication app = builder.Build();
+
+                // middleware
+                app.UseSerilogRequestLogging();
+
+                // configure the HTTP request pipeline
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseAuthorization();
+                app.MapControllers();
+
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Unhandled exception");
+            }
+            finally
+            {
+                Log.Information("Shut down complete");
+                Log.CloseAndFlush();
+            }
         }
     }
 }
---
+++ Cadmus.Graph.Api/Services/DatabaseSeedService.cs
@@ -0,0 +1,100 @@
+﻿using Cadmus.Graph.MySql;
+using Fusi.DbManager.MySql;
+using Polly;
+using System.Data.Common;
+
+namespace Cadmus.Graph.Api.Services
+{
+    /// <summary>
+    /// Database seed service.
+    /// See https://andrewlock.net/running-async-tasks-on-app-startup-in-asp-net-core-3.
+    /// </summary>
+    /// <seealso cref="IHostedService" />
+    public sealed class DatabaseSeedService : IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public DatabaseSeedService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        private static void SeedGraphDatabase(
+            IConfiguration config,
+            ILogger? logger)
+        {
+            // nope if database exists
+            string cst = config.GetConnectionString("Template");
+            string db = config.GetValue<string>("DatabaseName");
+
+            MySqlDbManager dbManager = new(cst);
+            if (dbManager.Exists(db))
+            {
+                logger?.LogInformation($"Database {db} exists");
+                return;
+            }
+
+            // else create and seed it
+            logger?.LogInformation($"Creating database {db}");
+            dbManager.CreateDatabase(db, MySqlGraphRepository.GetSchema(), null);
+        }
+
+        private static Task SeedGraphDatabaseAsync(IServiceProvider serviceProvider)
+        {
+            return Policy.Handle<DbException>()
+                .WaitAndRetry(new[]
+                {
+                    TimeSpan.FromSeconds(10),
+                    TimeSpan.FromSeconds(30),
+                    TimeSpan.FromSeconds(60)
+                }, (exception, timeSpan, _) =>
+                {
+                    ILogger? logger = serviceProvider
+                        .GetService<ILoggerFactory>()?
+                        .CreateLogger(typeof(DatabaseSeedService));
+
+                    string message = "Unable to connect to DB" +
+                        $" (sleep {timeSpan}): {exception.Message}";
+                    Console.WriteLine(message);
+                    logger?.LogError(exception, message);
+                }).Execute(() =>
+                {
+                    IConfiguration config =
+                        serviceProvider.GetService<IConfiguration>()!;
+
+                    ILogger? logger = serviceProvider
+                        .GetService<ILoggerFactory>()?
+                        .CreateLogger(typeof(DatabaseSeedService));
+
+                    Console.WriteLine("Seeding database...");
+                    SeedGraphDatabase(config, logger);
+                    Console.WriteLine("Seeding completed");
+                    return Task.CompletedTask;
+                });
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            IServiceProvider serviceProvider = scope.ServiceProvider;
+
+            try
+            {
+                await SeedGraphDatabaseAsync(serviceProvider);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                ILogger? logger = serviceProvider.GetService<ILoggerFactory>()!
+                    .CreateLogger(typeof(DatabaseSeedService));
+                logger.LogError(ex, ex.Message);
+                throw;
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
