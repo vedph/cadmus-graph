eef986ee3467816b563ab0686b0b94f1fde37e4b
Myrmex <dfusi@hotmail.com>
2023-02-01 10:31:54 +0100

    Update InstallToFeed.bat

--- InstallToFeed.bat
+++ InstallToFeed.bat
@@ -2,7 +2,7 @@
 echo PRESS ANY KEY TO INSTALL Cadmus Libraries TO LOCAL NUGET FEED
 echo Remember to generate the up-to-date package.
 pause
-c:\exe\nuget add .\Cadmus.Graph\bin\Debug\Cadmus.Graph.0.0.4.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Graph.Sql\bin\Debug\Cadmus.Graph.Sql.0.0.4.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Graph.MySql\bin\Debug\Cadmus.Graph.MySql.0.0.4.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Graph\bin\Debug\Cadmus.Graph.1.0.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Graph.Sql\bin\Debug\Cadmus.Graph.Sql.1.0.1.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Graph.MySql\bin\Debug\Cadmus.Graph.MySql.1.0.1.nupkg -source C:\Projects\_NuGet
 pause
