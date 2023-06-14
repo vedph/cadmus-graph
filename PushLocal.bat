a63b76f67696d1581421c75a5402ec1946914252
Myrmex <dfusi@hotmail.com>
2023-06-14 18:15:58 +0200

    Fixed mismatched reference

--- PushLocal.bat
+++ PushLocal.bat
@@ -6,7 +6,7 @@ echo You can now update Cadmus Core so that Cadmus.Index dependencies are update
 pause
 c:\exe\nuget add .\Cadmus.Graph.Ef\bin\Debug\Cadmus.Graph.Ef.2.2.10.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Graph.Ef.MySql\bin\Debug\Cadmus.Graph.Ef.MySql.2.2.10.nupkg -source C:\Projects\_NuGet
-c:\exe\nuget add .\Cadmus.Graph.Ef.PgSql\bin\Debug\Cadmus.Graph.Ef.PgSql.2.2.10.nupkg -source C:\Projects\_NuGet
+c:\exe\nuget add .\Cadmus.Graph.Ef.PgSql\bin\Debug\Cadmus.Graph.Ef.PgSql.2.2.11.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Graph.Sql\bin\Debug\Cadmus.Graph.Sql.2.2.10.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Graph.MySql\bin\Debug\Cadmus.Graph.MySql.2.2.10.nupkg -source C:\Projects\_NuGet
 c:\exe\nuget add .\Cadmus.Graph.Extras\bin\Debug\Cadmus.Graph.Extras.2.2.10.nupkg -source C:\Projects\_NuGet
